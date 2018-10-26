cd6fade79e6aa1f2cdd2d498d2f095dd75d78ac5
Serjio2888 <kerchtown@yandex.ru>
2018-10-26 16:37:21 +0300

    optimised

--- text_history.py
+++ text_history.py
@@ -12,25 +12,26 @@ class TextHistory:
     def text(self):
         return self._text
 
-    def insert(self, text, pos=None):
+    def check_errors(self, pos):
         if pos == None:
             pos = len(str(self._text))
         if pos > len(str(self._text)) or pos < 0:
-            raise ValueError
+            raise ValueError()
         self._version += 1
+        return pos
+    
+    def insert(self, text, pos=None):
+        pos = self.check_errors(pos)
         insert = InsertAction(text = text, pos = pos, from_version = (self._version-1),
                                to_version = self._version )
-        self._text = insert.apply(self._text)#
+        self._text = insert.apply(self._text)
         self._list.append(insert)
         return self._version
     
     def delete(self, length, pos=None):
-        if pos == None:
-            pos = len(str(self._text))
-        if (pos > len(str(self._text)) or pos < 0
-            or (pos+length)>len(str(self._text))):
-            raise ValueError
-        self._version += 1
+        pos = self.check_errors(pos)
+        if(pos+length)>len(str(self._text)):
+            raise ValueError()
         delete = DeleteAction(text = self._text, pos=pos, length=length,
                               from_version = (self._version-1), to_version = self._version )
         self._text = delete.apply(text=self._text)
@@ -38,11 +39,7 @@ class TextHistory:
         return self._version
 
     def replace(self, text, pos=None):
-        if pos == None:
-            pos = len(str(self._text))
-        if pos > len(str(self._text)) or pos < 0:
-            raise ValueError
-        self._version += 1
+        pos = self.check_errors(pos)
         replace = ReplaceAction(text = text, pos = pos, from_version = (self._version-1),
                                  to_version = self._version )
         self._text = replace.apply(self._text)
@@ -52,46 +49,48 @@ class TextHistory:
     def action(self, action):
         if ((action._from_version == action._to_version) or
             (self._version != action._from_version)):
-            raise ValueError
+            raise ValueError()
         self._version = action._to_version
         self._text = action.apply(self._text)
         differ = action._to_version - action._from_version
-        if (differ)>1:
+        if differ>1:
             for i in range(action._from_version, differ):
                 self._list.append(None)
         self._list.append(action)
         return self._version
-    
+
+    def helpIns(self, buff=[], i = 0):
+        for obj in self._list:
+            if i>0:
+                if type(obj)==type(buff[i-1])==InsertAction:
+                    if obj.pos == 0 and buff[i-1].pos == 0:
+                        (self._list[i]).text=(self._list[i]).text + (self._list[i-1]).text
+                        self._list[i-1]=None
+            i+=1
+            buff.append(obj)
+
+    def helpDel(self, buff=[], i=0):
+        for obj in self._list:
+            if i>0:
+                if type(obj)==type(buff[i-1])==DeleteAction:
+                    if obj.pos == 0 and buff[i-1].pos == 0:
+                        (self._list[i]).length=(self._list[i-1]).length+(self._list[i]).length
+                        self._list[i-1]=None
+            i+=1
+            buff.append(obj)
+            
     def get_actions(self, from_version = 0, to_version = None):
         if to_version == None:
             to_version = self._version
         if from_version < 0 or to_version > self._version or from_version > to_version:
-            raise ValueError
-        #В случае, если подряд идёт несколько операий insert в начале строки,
-        #заменяем их на одну, которая вставляет текст всех предыдущих. Остальные - None
-        buff = []
-        i = 0
-        for obj in self._list:
-            if i>0:
-                if obj.pos == 0 and buff[i-1].pos == 0 and type(obj)==type(buff[i-1])==InsertAction:
-                    (self._list[i]).text=(self._list[i]).text + (self._list[i-1]).text
-                    self._list[i-1]=None
-            i+=1
-            buff.append(obj)
-        #В случае, если подряд идёт несколько операий delete в начале строки,
-        #заменяем их на одну, которая удаляет текста на суммарную длину всех операций. Остальные - None
-        buff = []
-        i = 0
-        for obj in self._list:
-            if i>0:
-                if obj.pos == 0 and buff[i-1].pos == 0 and type(obj)==type(buff[i-1])==DeleteAction:
-                    (self._list[i]).length=(self._list[i-1]).length+(self._list[i]).length
-                    self._list[i-1]=None
-            i+=1
-            buff.append(obj)
+            raise ValueError()
+        self.helpIns()
+        self.helpDel()
         output = self._list[from_version:to_version]
+        for i in range(output.count(None)):
+            output.remove(None)
         return output
-
+    
 class Action:
     def __init__(self, pos=None, length=None, text='', from_version=None, to_version=None):
         self._pos = pos
@@ -99,6 +98,8 @@ class Action:
         self._text = text
         self._to_version = to_version
         self._from_version = from_version
+        if (self._from_version == self._to_version) or self._from_version > self._to_version:
+            raise ValueError()
 
     @property
     def text(self):
